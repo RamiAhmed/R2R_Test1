e343bac7204844051f4a76df7c1c4a5b33c606ed
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-11-24 13:37:23 +0100

    Removed stats-setting from start in gate of life
    - Since it would override stats set in Inspector

--- Assets/Scripts/GateOfLife.cs
+++ Assets/Scripts/GateOfLife.cs
@@ -6,21 +6,8 @@ public class GateOfLife : Entity {
 	// Use this for initialization
 	protected override void Start () {
 		base.Start();
-		
-		Name = "Gate of Life";
-		MaxHitPoints = 200f;
+
 		CurrentHitPoints = MaxHitPoints;
-		Armor = 10f;
-		
-		MovementSpeed = 0f;
-		AttacksPerSecond = 0.5f;
-		MinimumDamage = 5f;		
-		MaximumDamage = 30f;
-		Evasion = 0f;
-		Accuracy = 5f;
-		PerceptionRange = 5f;
-		AttackingRange = 5f;
-		FleeThreshold = 0f;
 	}
 	
 	// Update is called once per frame
